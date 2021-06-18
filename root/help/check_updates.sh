9f997b232aca5f302d3e8174c2484f0caf398661
TemhAAhmeT <wmaster@mailbox.org>
2021-06-18 18:19:36 +0200

    nginx error log

--- root/help/check_updates.sh
+++ root/help/check_updates.sh
@@ -15,6 +15,7 @@ rm -rf /usr/share/man/
 rm -rf /usr/share/locale/
 rm -rf /root/.cache
 rm -rf /var/log/*
+mkdir -p /var/log/nginx
 mkdir -p /var/log/mysql
 chown mysql:root /var/log/mysql
 touch /help/first_run_done
